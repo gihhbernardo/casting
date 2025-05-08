d497c75685f472fa37613dc3ec268f1207eeca0a
gihhbernardo <geovanna.bernardofrasao@gmail.com>
2025-05-08 09:51:10 -0300

    Add files via upload

---
+++ palavra.chave.py
@@ -0,0 +1,26 @@
+s= "geovanna"
+print(s[3])
+print(s[-3:])
+x=s.split("_")
+print(x)
+s= "geovanna"
+print("geovanna". replace("anna", "anne"))
+print(len("geovanna"))
+print(s.startswith("geo"))
+print("geovanna".upper())
+texto_escrito = "este livro é legal"
+outro_texto ='vou aprender a programar'
+outro_texto= texto_escrito
+print(outro_texto)
+primeiro_nome="geovanna"
+segundo_nome="bernardo"
+nome_completo= primeiro_nome+segundo_nome
+print(nome_completo)
+#nome=input()
+saudaçao="ola"+ s
+print(saudaçao)
+numero_um= input()
+numero_dois=input()
+resultado= int(numero_um) -int(numero_dois)
+print("resultado da operaçao "+str(resultado))
+
